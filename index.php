745af7053d143fe3c53d33ec0d7da3aa6b8a2214
Dmitry Bohdanov <rainsxng@gmail.com / starsettime@gmail.com>
2018-12-28 14:01:16 +0200

    index file deleted

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__.'/vendor/autoload.php';
-use Logger\LoggerClass;
-error_reporting(E_ALL & ~E_NOTICE);
-$logger = new LoggerClass();
-$logger->setLogFile('name.log');
-$logger->registerExceptionHandler();
-throw new Exception('sa');
-
